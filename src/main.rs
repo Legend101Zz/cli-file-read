6c4ac185b17e3e1af2aa02556aa4c7c725429af9
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-06 20:24:16 +0530

    added error handling code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,10 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn parse_config(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+return Err("not enough arguments")
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
