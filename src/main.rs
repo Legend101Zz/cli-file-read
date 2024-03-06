f82ffacd9847e371da3a9dca6450084b36c43d90
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-06 20:32:56 +0530

    added run fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -14,12 +15,19 @@ fn main() {
     println!("In file {}", config.filename);
 
     run(config)
+
+    if let Err(e) = run(config){
+        println!("Application error :{}",e);
+        process.exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text: \n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
